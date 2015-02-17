424f1d1ce2196a6f9b48093d04617a5372fa52a3
Simon Danisch <sdanisch@gmail.com>
2015-02-17 15:17:43 +0100

    first basic sketch

--- src/AbstractGPUArray.jl
+++ src/AbstractGPUArray.jl
@@ -1,5 +1,13 @@
 module AbstractGPUArray
 
-# package code goes here
+import Base: resize!, setindex!, getindex, map, foldl
+
+include("interface.jl")
+include("core.jl")
+
+export data
+export init_ram
+export resize
+
 
 end # module
